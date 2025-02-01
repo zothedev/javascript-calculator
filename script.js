552e10a9a79b02db59cf6d8c88b11a205037ad6e
zothedev <64105313+zothedev@users.noreply.github.com>
2025-02-01 14:09:32 -0500

    Create operate function

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ let divide = ((a,b) => a / b);
 let operand1;
 let operator;
 let operator2;
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
