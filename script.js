183ad765c3c239a9ab9efec07cee3227dd2fbecf
zothedev <64105313+zothedev@users.noreply.github.com>
2025-02-01 16:37:50 -0500

    Add calculator functionality for 2 operands only

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let add = ((a,b) => a + b);
-let subtract = ((a,b) => a - b);
-let multiply = ((a,b) => a * b);
-let divide = ((a,b) => a / b);
+let add = ((a, b) => a + b);
+let subtract = ((a, b) => a - b);
+let multiply = ((a, b) => a * b);
+let divide = ((a, b) => a / b);
 
 let operand1;
 let operator;
-let operator2;
+let operand2;
 
 function operate(num1, operator, num2) {
     switch (operator) {
@@ -34,14 +34,15 @@ calcContainer.addEventListener("click", (e) => {
     switch (target.id) {
         case 'clear':
             display.textContent = ""
+            operand1, operator, operator2 = "";
             break;
         case 'backspace':
-            display.textContent = display.textContent.slice(0,-1);
+            display.textContent = display.textContent.slice(0, -1);
             break;
         case '9':
             display.textContent += "9"
             break;
-        case '8':;
+        case '8': ;
             display.textContent += "8"
             break;
         case '7':
@@ -58,33 +59,53 @@ calcContainer.addEventListener("click", (e) => {
             break;
         case '4':
             display.textContent += "4"
-            break;   
+            break;
         case 'multiply':
             display.textContent += "*"
-            break;  
+            break;
         case '3':
             display.textContent += "3"
-            break;  
+            break;
         case '2':
             display.textContent += "2"
-            break;  
+            break;
         case '1':
             display.textContent += "1"
-            break; 
+            break;
         case 'subtract':
             display.textContent += "-"
-            break;  
+            break;
         case '.':
             display.textContent += "."
             break;
         case '0':
             display.textContent += "0"
-            break; 
+            break;
         case 'equals':
-            // operate()
-            break; 
+            populateVars(display.textContent);
+            display.textContent = operate(operand1, operator, operand2);
+            break;
         case 'add':
             display.textContent += "+"
-            break; 
+            break;
     }
 });
+
+function populateVars(str) {
+    let operandIndex;
+    if (str.indexOf("/") > 0) {
+        operandIndex = str.indexOf("/");
+    } else if (str.indexOf("*") > 0) {
+        operandIndex = str.indexOf("*");
+    } else if (str.indexOf("+") > 0) {
+        operandIndex = str.indexOf("+");
+    } else {
+        operandIndex = str.indexOf("-");
+    }
+    // console.log(str);
+    operand1 = +(str.slice(0, operandIndex));
+    operand2 = +(str.slice(operandIndex+1));
+    operator = str.slice(operandIndex,operandIndex+1);
+    // console.log(`${operand1} ${operator} ${operand2}`);
+}
+
