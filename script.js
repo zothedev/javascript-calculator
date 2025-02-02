699878f0ca85dca6b7f3209d5004f44323e57acf
zothedev <64105313+zothedev@users.noreply.github.com>
2025-02-02 13:48:36 -0500

    Reconfigure calculator logic once again

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
+// todo:
+// - disable equals button if two operands are not present (num2 becomes 0 it seems)
+// - fix divide by 0 bug
+// - 
+
 let add = ((a, b) => a + b);
 let subtract = ((a, b) => a - b);
 let multiply = ((a, b) => a * b);
 let divide = ((a, b) => a / b);
 
-let ops = ["+", "-", "/", "*"];
-let num1;
-let operator;
-let num2;
+let num1 = "";
+let operator = "";
+let num2 = "";
 
 function operate(num1, operator, num2) {
+    if (!num1) {
+        console.log("first operator clicked");
+        return num2;
+    }
+
+
     switch (operator) {
         case "+":
-            return add(num1, num2);
+            operator = "";
+            return add(+num1, +num2).toString();
         case "-":
-            return subtract(num1, num2);
+            operator = "";
+            return subtract(+num1, +num2);
         case "*":
-            return multiply(num1, num2);
+            operator = "";
+            return multiply(+num1, +num2);
         case "/":
-            return divide(num1, num2);
+            operator = "";
+            return divide(+num1, +num2);
     }
 }
 
@@ -26,103 +40,110 @@ let display = calcContainer.querySelector(".display");
 
 calcContainer.addEventListener("click", (e) => {
     let target = e.target;
+    
 
     switch (target.id) {
         case 'clear':
-            display.textContent = ""
-            num1, operator, num2 = "";
+            num1 = ""; 
+            operator = ""; 
+            num2 = "";
+            display.textContent = "";
             break;
-        case 'backspace':
-            display.textContent = display.textContent.slice(0, -1);
-            break;
+        // case 'backspace':
+        //     display.textContent = display.textContent.slice(0, -1);
+        //     break;
         case '9':
-            display.textContent += "9"
+            num2 += "9";
+            display.textContent = num1 + operator + num2;
             break;
         case '8': ;
-            display.textContent += "8"
+            num2 += "8";
+            display.textContent = num1 + operator + num2;
             break;
         case '7':
-            display.textContent += "7"
-            break;
-        case 'divide':
-            // if there is already an expression in display (if an operator is present in expression)
-            if (Number.isNaN(+(display.textContent))) {
-                assignNums(display.textContent);
-                display.textContent = operate(num1, operator, num2);
-            }
-            display.textContent += "/"
+            num2 += "7";
+            display.textContent = num1 + operator + num2;
             break;
         case '6':
-            display.textContent += "6"
+            num2 += "6";
+            display.textContent = num1 + operator + num2;
             break;
         case '5':
-            display.textContent += "5"
+            num2 += "5";
+            display.textContent = num1 + operator + num2;
             break;
         case '4':
-            display.textContent += "4"
-            break;
-        case 'multiply':
-            // if there is already an expression in display (if an operator is present in expression)
-            if (Number.isNaN(+(display.textContent))) {
-                assignNums(display.textContent);
-                display.textContent = operate(num1, operator, num2);
-            }
-            display.textContent += "*"
+            num2 += "4";
+            display.textContent = num1 + operator + num2;
             break;
         case '3':
-            display.textContent += "3"
+            num2 += "3";
+            display.textContent = num1 + operator + num2;
             break;
         case '2':
-            display.textContent += "2"
+            num2 += "2";
+            display.textContent = num1 + operator + num2;
             break;
         case '1':
-            display.textContent += "1"
+            num2 += "1";
+            display.textContent = num1 + operator + num2;
             break;
-        case 'subtract':
-            // if there is already an expression in display (if an operator is present in expression)
-            if (Number.isNaN(+(display.textContent))) {
-                assignNums(display.textContent);
-                display.textContent = operate(num1, operator, num2);
-            }
-            display.textContent += "-"
-            break;
-        case '.':
-            display.textContent += "."
-            break;
+        // case '.':
+        //     display.textContent += "."
+        //     break;
         case '0':
             display.textContent += "0"
             break;
-        case 'equals':
-            assignNums(display.textContent);
-            display.textContent = operate(num1, operator, num2);
-            break;
+        
+        
         case 'add':
-            // if there is already an expression in display (if an operator is present in expression)
-            if (Number.isNaN(+(display.textContent))) {
-                assignNums(display.textContent);
-                display.textContent = operate(num1, operator, num2);
+            if (!num2) {
+                break;
             }
-            display.textContent += "+";
+            operator = "+";
+            num1 = operate(num1, operator, num2);
+            num2 = "";
+            display.textContent = num1 + operator + num2;
             break;
+        case 'subtract':
+            if (!num2) {
+                break;
+            }
+            operator = "-";
+            num1 = operate(num1, operator, num2);
+            num2 = "";
+            display.textContent = num1 + operator + num2;
+            break;
+        case 'multiply':
+            if (!num2) {
+                break;
+            }
+            operator = "*";
+            num1 = operate(num1, operator, num2);
+            num2 = "";
+            display.textContent = num1 + operator + num2;
+            break;
+        case 'divide':
+            if (!num2) {
+                break;
+            }
+            operator = "/";
+            num1 = operate(num1, operator, num2);
+            num2 = "";
+            display.textContent = num1 + operator + num2;
+            break;
+        case 'equals':
+            // make sure two operands are present
+            if (!operator || !num2) {
+                break;
+            }
+            num1 = operate(num1, operator, num2);
+            num2 = num1;
+            num1 = ""
+            operator = "";
+            display.textContent = num1 + operator + num2;
+            break;
+
+        
     }
 });
-
-function assignNums(str) {
-    let operatorIndex = "";
-    let i = 0;
-
-    if (str.indexOf("*") > -1) {
-        operatorIndex = str.indexOf("*");
-    } else if (str.indexOf("/") > -1) {
-        operatorIndex = str.indexOf("/");
-    } else if (str.indexOf("+") > -1) {
-        operatorIndex = str.indexOf("+");
-    } else if (str.indexOf("-") > -1) {
-        operatorIndex = str.indexOf("-");
-    }
-    
-    num1 = +(str.slice(0,operatorIndex));
-    num2 = +(str.slice(operatorIndex+1));
-    operator = str[operatorIndex];
-    // console.log(`${num1} ${operation}`)
-}
